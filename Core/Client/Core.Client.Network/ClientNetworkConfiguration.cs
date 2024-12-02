a3f5cac5f29c7830aa7aefdfc628eaecf5f124b1
Copkiller <felipe_157@windowslive.com>
2024-12-01 22:22:54 -0400

    Changing the default configuration of each service, so that it starts as Enabled = false, and then starts correctly.

--- Core/Client/Core.Client.Network/ClientNetworkConfiguration.cs
+++ Core/Client/Core.Client.Network/ClientNetworkConfiguration.cs
@@ -5,7 +5,7 @@ namespace Core.Client.Network;
 public class ClientNetworkConfiguration : IServiceConfiguration
 {
     public Type ServiceType { get; } = typeof(ClientNetworkService);
-    public bool Enabled { get; set; } = true;
+    public bool Enabled { get; set; } = false;
     public bool NeedUpdate { get; set; } = true;
     public int UpdateIntervalMs { get; set; } = 15;
 }
--- Core/Server/Core.Server.Network/ServerNetworkConfiguration.cs
+++ Core/Server/Core.Server.Network/ServerNetworkConfiguration.cs
@@ -5,7 +5,7 @@ namespace Core.Server.Network;
 public class ServerNetworkConfiguration : IServiceConfiguration
 {
     public Type ServiceType { get; } = typeof(ServerNetworkService);
-    public bool Enabled { get; set; } = true;
+    public bool Enabled { get; set; } = false;
     public bool NeedUpdate { get; set; } = true;
     public int UpdateIntervalMs { get; set; } = 15;
 }
