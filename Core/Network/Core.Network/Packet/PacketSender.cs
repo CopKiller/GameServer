5cb82715405fcdf0844272aab072b5d5bed1c63b
Copkiller <felipe_157@windowslive.com>
2024-12-06 23:33:53 -0400

    Fix Bug in sender, set manual.

--- Core/Network/Core.Network/Packet/PacketSender.cs
+++ Core/Network/Core.Network/Packet/PacketSender.cs
@@ -8,54 +8,88 @@ namespace Core.Network.Packet;
 
 public class PacketSender(NetPacketProcessor packetProcessor, IConnectionManager manager) : IPacketSender
 {
-    public void SendPacket<TPacket>(ICustomNetPeer peer, TPacket packet,
-        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered) where TPacket : class, new()
-    {
-        if (peer is not CustomNetPeer liteNetPeer)
-            throw new InvalidOperationException("Invalid peer type. Expected CustomNetPeer.");
+    [ThreadStatic] // Evita problemas de concorrência
+    private static CustomDataWriter? _writer;
 
-        packetProcessor.Send(liteNetPeer.Peer, packet, Extensions.ConvertToLiteDeliveryMethod(deliveryMethod));
+    private CustomDataWriter Writer => _writer ??= new CustomDataWriter();
+
+    public void SendPacket<TPacket>(
+        ICustomNetPeer peer,
+        TPacket packet,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    ) where TPacket : class, new()
+    {
+        var writer = Writer;
+        writer.Reset();
+        packetProcessor.Write(writer.GetNetDataWriter(), packet); // Usa o NetPacketProcessor diretamente
+        peer.Send(writer, deliveryMethod);
     }
 
-    public void SendPacket<T>(ICustomNetPeer peer, ref T packet,
-        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered) where T : ICustomSerializable
+    public void SendPacket<T>(
+        ICustomNetPeer peer,
+        ref T packet,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    ) where T : ICustomSerializable
     {
-        if (peer is not CustomNetPeer liteNetPeer)
-            throw new InvalidOperationException("Invalid peer type. Expected CustomNetPeer.");
-
-        var adapter = new LiteNetSerializableAdapter<T>(packet);
-        packetProcessor.SendNetSerializable(liteNetPeer.Peer, ref adapter,
-            Extensions.ConvertToLiteDeliveryMethod(deliveryMethod));
+        var writer = Writer;
+        writer.Reset();
+        SerializePacket(ref packet, writer); // Centraliza a lógica de serialização
+        peer.Send(writer, deliveryMethod);
     }
 
-    public void SendPacket(ICustomNetPeer peer, byte[] data,
-        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered)
+    public void SendPacket(
+        ICustomNetPeer peer,
+        byte[] data,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    )
     {
-        peer.Send(data, deliveryMethod);
+        peer.Send(data, deliveryMethod); // Pacotes simples já estão prontos
     }
 
-    public void SendPacketToAll<TPacket>(TPacket packet,
-        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered) where TPacket : class, new()
+    public void SendPacketToAll<TPacket>(
+        TPacket packet,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    ) where TPacket : class, new()
     {
-        var allPeers = manager.CustomPeers.Values;
+        var writer = Writer;
+        writer.Reset();
+        packetProcessor.Write(writer.GetNetDataWriter(), packet); // Serializa uma vez
 
-        foreach (var peer in allPeers) SendPacket(peer, packet, deliveryMethod);
+        foreach (var peer in manager.CustomPeers.Values)
+        {
+            peer.Send(writer, deliveryMethod);
+        }
     }
 
-    public void SendPacketToAll<T>(ref T packet,
-        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered) where T : ICustomSerializable
+    public void SendPacketToAll<T>(
+        ref T packet,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    ) where T : ICustomSerializable
     {
-        var allPeers = manager.CustomPeers.Values;
+        var writer = Writer;
+        writer.Reset();
+        SerializePacket(ref packet, writer); // Serializa uma vez
 
-        var adapter = new LiteNetSerializableAdapter<T>(packet);
-
-        foreach (var peer in allPeers) SendPacket(peer, adapter, deliveryMethod);
+        foreach (var peer in manager.CustomPeers.Values)
+        {
+            peer.Send(writer, deliveryMethod);
+        }
     }
 
-    public void SendPacketToAll(byte[] data, CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered)
+    public void SendPacketToAll(
+        byte[] data,
+        CustomDeliveryMethod deliveryMethod = CustomDeliveryMethod.ReliableOrdered
+    )
     {
-        var allPeers = manager.CustomPeers.Values;
+        foreach (var peer in manager.CustomPeers.Values)
+        {
+            peer.Send(data, deliveryMethod);
+        }
+    }
 
-        foreach (var peer in allPeers) peer.Send(data, deliveryMethod);
+    private void SerializePacket<T>(ref T packet, CustomDataWriter writer) where T : ICustomSerializable
+    {
+        packetProcessor.WriteHash<T>(writer.GetNetDataWriter());
+        packet.Serialize(writer);
     }
 }
