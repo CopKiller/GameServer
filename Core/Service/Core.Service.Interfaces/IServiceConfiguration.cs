839746eec437e2069c74946c47b5a0521338da26
Copkiller <felipe_157@windowslive.com>
2024-12-15 15:47:45 -0400

    Add summary in IServiceConfiguration: Implement this interface in a class that will be used to configure a service. The service configuration will be used to enable/disable the service, set the update interval and other settings.

--- Core/Service/Core.Service.Interfaces/IServiceConfiguration.cs
+++ Core/Service/Core.Service.Interfaces/IServiceConfiguration.cs
@@ -1,7 +1,10 @@
-using Core.Service.Interfaces.Types;
 
 namespace Core.Service.Interfaces;
 
+/// <summary>
+/// Implement this interface in a class that will be used to configure a service.
+/// The service configuration will be used to enable/disable the service, set the update interval and other settings.
+/// </summary>
 public interface IServiceConfiguration
 {
     Type ServiceType { get; }
