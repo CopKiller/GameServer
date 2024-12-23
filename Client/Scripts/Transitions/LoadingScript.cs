411cd3c371f1d15b71ed97a7f2082d3639726333
Copkiller <felipe_157@windowslive.com>
2024-12-23 01:37:49 -0400

    Added basic logic for loading with percentage visibility

--- Client/Scripts/Transitions/LoadingScript.cs
+++ Client/Scripts/Transitions/LoadingScript.cs
@@ -1,15 +1,50 @@
-using Godot;
 using System;
+using Core.Client.Resource;
+using Godot;
+
+namespace Game.Scripts.Transitions;
+
+using Godot;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public partial class LoadingScript : Control
 {
-	// Called when the node enters the scene tree for the first time.
+	private ProgressBar? _progressBar;
+	
+	private readonly List<Task> _loadingSteps = [];
+
 	public override void _Ready()
 	{
+		_progressBar = GetNode<ProgressBar>("ProgressBar");
+	}
+	
+	public void AddTask(Action task)
+	{
+		_loadingSteps.Add(new Task(task));
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public async Task StartLoading(Action? onLoadingComplete = null)
 	{
+		int totalSteps = _loadingSteps.Count;
+		int currentStep = 0;
+		
+		if (_progressBar == null)
+		{
+			GD.PrintErr("ProgressBar não encontrado!");
+			return;
+		}
+
+		foreach (var step in _loadingSteps)
+		{
+			await step;
+			
+			currentStep++;
+			_progressBar.Value = (currentStep / (float)totalSteps) * 100;
+		}
+
+		GD.Print("Carregamento concluído!");
+		
+		onLoadingComplete?.Invoke();
 	}
 }
