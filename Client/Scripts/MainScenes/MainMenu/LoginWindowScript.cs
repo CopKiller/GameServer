c1ebc4225baf6536684b29213ea52cb6504f4437
Copkiller <felipe_157@windowslive.com>
2024-12-26 23:47:22 -0400

    Changing the name of the login window, it was Start Windows, now it is Login Window, and changing the name to its respective equivalents.

--- Client/Scripts/MainScenes/MainMenu/LoginWindowScript.cs
+++ Client/Scripts/MainScenes/MainMenu/LoginWindowScript.cs
@@ -3,7 +3,7 @@ using Godot;
 
 namespace Game.Scripts.MainScenes.MainMenu;
 
-public partial class StartWindowScript : WindowBase
+public partial class LoginWindowScript : WindowBase
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
