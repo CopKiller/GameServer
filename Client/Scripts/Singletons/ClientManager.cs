fdd4786d341a12befc2abab5cf7e9701a117b6f9
Copkiller <felipe_157@windowslive.com>
2024-12-27 23:26:24 -0400

    Removed responsibilities, to improve cohesion and syntax.

--- Client/Scripts/Singletons/ClientManager.cs
+++ Client/Scripts/Singletons/ClientManager.cs
@@ -1,47 +1,42 @@
 using System;
+using System.Threading.Tasks;
 using Core.Client.Extensions;
 using Core.Service.Interfaces.Types;
 using Game.Scripts.Extensions;
 using Game.Scripts.Extensions.Services;
+using Game.Scripts.Transitions;
 using Godot;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Game.Scripts.Singletons;
 
+/// <summary>
+/// Gerencia o estado do cliente no jogo.
+/// Este nó deve ser o último Autoload a ser carregado no projeto.
+/// Sua principal responsabilidade é iniciar o processo de carregamento do jogo
+/// após todos os Autoloads terem adicionado seus itens de carregamento.
+/// Também permite a adição de novos itens de carregamento antes de iniciar o processo.
+/// </summary>
+
 public partial class ClientManager : Node
 {
-    private readonly IServiceManager? _serviceManager;
-    
-    private PackedScene? _loadingScene;
-    private PackedScene? _splashScreen;
-    
-    public ClientManager()
-    {
-        var services = new ServiceCollection();
-        services.AddLogger();
-        services.AddGodotLoggerOutput();
-        services.AddServiceManager();
-        
-        var provider = services.BuildServiceProvider();
-        
-        _serviceManager = provider.GetService<IServiceManager>();
-        
-        _serviceManager?.Register();
-        _serviceManager?.Start();
-    }
-    
     public override void _Ready()
     {
+        var loadingScript = GetTree().CurrentScene as LoadingScript;
+        // Adicionar itens de carregamento aqui caso necessário...
+        // Outros nós podem adicionar itens no carregamento, mas apenas este vai iniciar o carregamento no método abaixo.
         
+        
+        loadingScript?.StartLoading(openNextScene: true);
     }
     
-    private void ChangeScene(PackedScene scene)
+    public void ExitGame()
     {
-        var result = GetTree().ChangeSceneToPacked(scene);
-        
-        if (result != Error.Ok)
-        {
-            GD.PrintErr("Erro ao carregar cena!");
-        }
+        GetTree().Quit();
+    }
+    
+    public override void _ExitTree()
+    {
+        // Liberar recursos do ClientManager...
     }
 }
