79ea168a27624d4db47fd2b6d5abe512b3a8d68c
Copkiller <felipe_157@windowslive.com>
2025-01-02 02:08:46 -0400

    Adicionado um autoload(singleton), que gerencia a cena de carregamento

---
+++ Client/Scripts/Singletons/LoadingManager.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Game.Scripts.Extensions;
+using Game.Scripts.Transitions;
+using Godot;
+
+namespace Game.Scripts.Singletons;
+
+public partial class LoadingManager : Node
+{
+    private PackedScene? _loadingPackedScene;
+    
+    private LoadingScript? _loadingScene;
+
+    private readonly Queue<(Func<Task>, string)> _tasks = new();
+    private bool _isLoading;
+
+    [Signal]
+    public delegate void LoadingCompleteEventHandler();
+
+    public void AddTask(Func<Task> task, string description) => _tasks.Enqueue((task, description));
+
+    public override void _Ready()
+    {
+        GD.Print("LoadingManager ready!");
+        
+        var sceneManager = ServiceManager.GetRequiredService<SceneManager>();
+        _loadingPackedScene = sceneManager.LoadScenePacked<LoadingScript>();
+    }
+
+    public override void _ExitTree()
+    {
+        ResetLoading();
+    }
+
+    public async Task StartLoading(Func<Task>? nextSceneTask = null)
+    {
+        if (_isLoading) return;
+        _isLoading = true;
+
+        if (_loadingPackedScene == null)
+        {
+            GD.PrintErr("Loading scene not found!");
+            return;
+        }
+
+        var progress = 0;
+        var totalTasks = _tasks.Count;
+
+        if (totalTasks == 0)
+        {
+            GD.Print("Nenhuma tarefa para carregar!");
+            return;
+        }
+
+        _loadingScene = _loadingPackedScene.Instantiate<LoadingScript>();
+        await AddChildAsync(_loadingScene);
+
+        while (_tasks.Count > 0)
+        {
+            var (task, description) = _tasks.Dequeue();
+            try
+            {
+                GD.Print($"Iniciando: {description}");
+                _loadingScene.UpdateMessage(description);
+                await task();
+            }
+            catch (Exception ex)
+            {
+                GD.PrintErr($"Erro: {description} - {ex.Message}");
+            }
+
+            progress++;
+            await _loadingScene.SmoothUpdateProgressBar(progress / (float)totalTasks, 0.5f);
+        }
+        
+        _loadingScene.UpdateMessage("Carregamento completo!");
+        await _loadingScene.SmoothUpdateProgressBar(progress / (float)totalTasks, 0.5f);
+
+        await _loadingScene.FadeOut(0.5f);
+        
+        if (nextSceneTask != null)
+            await nextSceneTask();
+
+        EmitSignal(SignalName.LoadingComplete);
+
+        ResetLoading();
+    }
+
+    private Task AddChildAsync(Node child)
+    {
+        var tcs = new TaskCompletionSource();
+        
+        child.Connect(Node.SignalName.Ready, Callable.From(OnReady));
+        
+        GetTree().Root.CallDeferred(Node.MethodName.AddChild, child);
+        
+        return tcs.Task;
+        
+        void OnReady()
+        {
+            tcs.TrySetResult();
+        }
+    }
+
+    private void ResetLoading()
+    {
+        _isLoading = false;
+        _loadingScene?.QueueFree();
+    }
+}
--- Client/Scripts/Transitions/LoadingScript.cs
+++ Client/Scripts/Transitions/LoadingScript.cs
@@ -3,133 +3,69 @@ using Godot;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Game.Scripts.Extensions;
+using Game.Scripts.Extensions.Attributes;
 
 namespace Game.Scripts.Transitions
 {
+    [ScenePath("res://Client/Scenes/Transitions/LoadingScene.tscn")]
     public partial class LoadingScript : Control
     {
         private TextureRect? _background;
         private ProgressBar? _progressBar;
         private Label? _lblTaskName;
         private Tween? _tween;
-        
-        [Export] public PackedScene? NextScene { get; set; }
-
-        private readonly List<(Func<Task>, string?)> _loadingSteps = [];
 
         public override void _Ready()
         {
+            GD.Print("LoadingScript ready!");
+            
             _background = GetNode<TextureRect>("%Background");
             _progressBar = GetNode<ProgressBar>("%ProgressBar");
             _lblTaskName = GetNode<Label>("%lblTaskName");
+
+            if (_background != null && _progressBar != null && _lblTaskName != null) return;
             
-            if (_background == null || _progressBar == null || _lblTaskName == null)
-            {
-                GD.PrintErr("Componentes obrigatórios não foram encontrados!");
-                return;
-            }
-        }
-
-        public void AddTask(Func<Task> task, string? taskName = null)
-        {
-            _loadingSteps.Add((task, taskName));
-        }
-
-        public async void StartLoading(Action? onLoadingComplete = null, bool openNextScene = true)
-        {
-            await this.FadeIn();
-            
-            int totalSteps = _loadingSteps.Count;
-            int currentStep = 0;
-
-            if (_progressBar == null)   
-            {
-                GD.PrintErr("ProgressBar não encontrado!");
-                return;
-            }
-
-            foreach (var step in _loadingSteps)
-            {
-                if (_lblTaskName != null)
-                    _lblTaskName.Text = step.Item2;
-
-                try
-                {
-                    await step.Item1();
-                    GD.Print($"Tarefa concluída: {step.Item2}");
-                }
-                catch (Exception ex)
-                {
-                    GD.PrintErr($"Erro ao executar tarefa: {step.Item2}. Exception: {ex.Message}");
-                }
-                
-                currentStep++;
-                double targetValue = (currentStep / (float)totalSteps) * _progressBar.MaxValue;
-                
-                await SmoothUpdateProgressBar(targetValue, 0.5f);
-            }
-
-            if (_lblTaskName != null)
-                _lblTaskName.Text = "Carregamento concluído!";
-
-            await Task.Delay(200);
-            
-            await this.FadeOut();
-            
-            if (openNextScene)
-            {
-                if (NextScene != null)
-                    GetTree().ChangeSceneToPacked(NextScene);
-                else
-                    GD.PrintErr("Próxima cena não foi definida!");
-            }
-            
-            onLoadingComplete?.Invoke();
-        }
-
-        private async Task SmoothUpdateProgressBar(double targetValue, float duration)
-        {
-            _tween = CreateTween();
-
-            if (_progressBar == null || _tween == null) return;
-
-            _tween.SetEase(Tween.EaseType.InOut);
-            _tween.SetTrans(Tween.TransitionType.Cubic);
-            _tween.TweenProperty(_progressBar, "value", targetValue, duration);
-            await ToSignal(_tween, Tween.SignalName.Finished);
+            GD.PrintErr("Componentes obrigatórios não foram encontrados!");
         }
         
-        private void TestAddTask()
+        public void UpdateMessage(string message)
         {
-            AddTask(async () =>
+            if (_lblTaskName == null)
             {
-                await Task.Delay(200);
-            }, "Teste 1");
+                GD.PrintErr("Label para TaskName não foi encontrada.");
+                return;
+            }
 
-            AddTask(async () =>
-            {
-                await Task.Delay(100);
-            }, "Teste 2");
+            _lblTaskName.Text = message;
+        }
 
-            AddTask(async () =>
+        public async Task SmoothUpdateProgressBar(double targetValue, float duration)
+        {
+            if (_progressBar == null)
             {
-                await Task.Delay(1000);
-            }, "Teste 3");
-            
-            AddTask(async () =>
+                GD.PrintErr("ProgressBar não foi encontrada.");
+                return;
+            }
+
+            _tween = CreateTween();
+
+            if (_tween == null)
             {
-                await Task.Delay(600);
-            }, "Teste 4");
-            
-            AddTask(async () =>
+                GD.PrintErr("Falha ao criar Tween.");
+                return;
+            }
+
+            try
             {
-                await Task.Delay(300);
-            }, "Teste 5");
-            
-            AddTask(async () =>
+                _tween.SetEase(Tween.EaseType.InOut);
+                _tween.SetTrans(Tween.TransitionType.Cubic);
+                _tween.TweenProperty(_progressBar, "value", targetValue * _progressBar.MaxValue, duration);
+                await ToSignal(_tween, Tween.SignalName.Finished);
+            }
+            catch (Exception e)
             {
-                await Task.Delay(1000); 
-            }, "Teste 6");
+                GD.PrintErr($"Erro ao configurar Tween: {e.Message}");
+            }
         }
     }
 }
