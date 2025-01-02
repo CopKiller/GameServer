f7d92da14c35cbec0a709bcc1cb78ded5a591e3c
Copkiller <felipe_157@windowslive.com>
2025-01-02 02:16:00 -0400

    Alteração no registrador de pacotes, para não ocorrer duplicatas e problemas no serviço externo.

--- Core/Network/Core.Network/Packet/PacketRegister.cs
+++ Core/Network/Core.Network/Packet/PacketRegister.cs
@@ -6,8 +6,16 @@ namespace Core.Network.Packet;
 
 public class PacketRegister(NetPacketProcessor packetProcessor) : IPacketRegister
 {
+    private readonly HashSet<Type> nestedTypes = new();
     public void RegisterPacket<TPacket>(Action<TPacket, ICustomNetPeer> onReceive) where TPacket : class, new()
     {
+        if (nestedTypes.Contains(typeof(TPacket)))
+        {
+            return;
+        }
+        
+        nestedTypes.Add(typeof(TPacket));
+        
         packetProcessor.SubscribeReusable(onReceive);
     }
 
