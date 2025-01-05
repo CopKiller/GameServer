fe0e71a855236d03a9174f8292984f89124419cb
Copkiller <felipe_157@windowslive.com>
2025-01-05 19:18:51 -0400

    Modificações de nomes, para refletir a sintaxe clara de adaptadores de libs externas, e nomes que estavam pouco descritivos.

--- Core/Network/Core.Network.SerializationObjects/AccountDto.cs
+++ Core/Network/Core.Network.SerializationObjects/AccountDto.cs
@@ -2,21 +2,21 @@
 
 namespace Core.Network.SerializationObjects;
 
-public class AccountDto : ICustomSerializable
+public class AccountDto : IAdapterSerializable
 {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public List<PlayerDto?> Players { get; set; } = new();
 
-    public void Serialize(ICustomDataWriter writer)
+    public void Serialize(IAdapterDataWriter writer)
     {
         writer.Put(Id);
         writer.Put(Username);
         writer.Put(Email);
     }
 
-    public void Deserialize(ICustomDataReader reader)
+    public void Deserialize(IAdapterDataReader reader)
     {
         Id = reader.GetInt();
         Username = reader.GetString();
